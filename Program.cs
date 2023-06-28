f9a467945e0e42ff2c12df2abe908cbeef3900ac
MIkhail <MDEvteshin@yandex.ru>
2023-06-28 19:08:55 +0400

    3

--- Program.cs
+++ Program.cs
@@ -34,31 +34,78 @@
 //     Console.WriteLine();
 // }
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
-// стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-System.Console.WriteLine($"Enter size of array");
-int a = Convert.ToInt32(Console.ReadLine());
-int [] randomArray = new int[a];
+// // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// // стоящих на нечётных позициях.
+// // [3, 7, 23, 12] -> 19
+// // [-4, -6, 89, 6] -> 0
+// System.Console.WriteLine($"Enter size of array");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int [] randomArray = new int[a];
 
-void ar (int a){
-    for (int i = 0; i < a; i++)
-    {
-        randomArray[i] = new Random().Next (1,9);
-        System.Console.Write(randomArray[i] + " ");
-    }
-}
-int kol(int[] randomArray)
-    {
-    int sum = 0;
-    int i = 0;
-    while (i < randomArray.Length)
+// void ar (int a){
+//     for (int i = 0; i < a; i++)
+//     {
+//         randomArray[i] = new Random().Next (1,9);
+//         System.Console.Write(randomArray[i] + " ");
+//     }
+// }
+// int kol(int[] randomArray)
+//     {
+//     int sum = 0;
+//     int i = 0;
+//     while (i < randomArray.Length)
+//         {
+//             sum = sum + randomArray[i];
+//             i = i + 2;
+//         }
+//         return sum;
+//     }
+// ar(a);
+// System.Console.WriteLine($"Sum elements which staied on non even positions: {kol(randomArray)}");
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+Console.WriteLine("Enter da array");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("This is your array: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
         {
-            sum = sum + randomArray[i];
-            i = i + 2;
+            max = numbers[z];
         }
-        return sum;
-    }
-ar(a);
-System.Console.WriteLine($"Sum elements which staied on non even positions: {kol(randomArray)}");
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
+
+Console.WriteLine($"Total numbers {numbers.Length}. Max number = {max}, Min number = {min}");
+Console.WriteLine($"Difference between max and min = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
